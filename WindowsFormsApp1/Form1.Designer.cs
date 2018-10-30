667643af68af491e3604cc343698b7c3ebfedd87
MajorMarcin <zelkowski.m@gmail.com>
2018-10-30 03:25:26 +0100

    program

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿using System;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Szukaj pliku";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 64);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(468, 132);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(132, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "szyfruj";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(213, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "odszyfruj";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(294, 15);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(56, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(365, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Klucz:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 202);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(468, 117);
+            this.textBox3.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 335);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Marcin Zelkowski";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public StringBuilder sb = new StringBuilder();
+        public int klucz;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                label1.Text = openFileDialog1.FileName;
+                byte[] fileBytes = File.ReadAllBytes(label1.Text);
+                string napis = Encoding.ASCII.GetString(fileBytes);
+                foreach (byte b in fileBytes)
+                textBox1.Text = napis.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog1.FileName, textBox1.Text);
+            }
+        }
+        
+        public void button3_Click(object sender, EventArgs e)
+        {
+            klucz = System.Convert.ToInt32(textBox2.Text);
+            label2.Text = "Obecnie użyty klucz:" + textBox2.Text;
+            textBox3.Text = Encrypt(klucz, textBox1.Text);
+        }
+
+        public static string Encrypt(int klucz, string dokodowania)
+        {
+            List<string> lista = new List<string>();
+            for (int i = 0; i < klucz; i++)
+            {
+                lista.Add("");
+                //init
+            }
+        //początek
+            int wiersz = 0;
+            int kierunek = 1;
+            foreach (char c in dokodowania)
+            {
+        //zmiana kierunku po natrafia na góre i dół płotka
+                if (wiersz + kierunek == klucz || wiersz + kierunek == -1)
+                {
+                    kierunek = kierunek * (-1);
+                }
+                
+                lista[wiersz] += c;//dodanie do danego wiersza(stringa) kolejnego znaku
+                wiersz += kierunek;//wskazanie kolejnego wiersza
+            }
+
+            //zrzucanie wartości z listy do jednego napisu
+            string tekst = "";
+            foreach (string s in lista)
+            {
+                tekst += s;
+            }
+
+            return tekst;
+        }
+
+        public static string Decrypt(int klucz, string zakodowany)
+        {
+            int rozmiar = zakodowany.Length;
+            List<List<int>> lista = new List<List<int>>();//podwójna lista
+            for (int i = 0; i < klucz; i++)
+            {
+                lista.Add(new List<int>());
+            }
+
+            int wiersz = 0;
+            int kierunek = 1;
+            //wrzucanie wskażników do listy z poprawką na rozmiar żeby wiedzieć gdzie koniec
+            for (int i = 0; i < rozmiar; i++)
+            {
+                //tak jak wyżej zmiana znaku po trafieniu w krawędzie siatki/płotka
+                if (wiersz + kierunek == klucz || wiersz + kierunek == -1)
+                {
+                    kierunek = kierunek * (-1);
+                }
+                lista[wiersz].Add(i);//numerowanie pozycji dla liter
+                wiersz += kierunek;
+            }
+
+            int licznik = 0;
+            char[] buffer = new char[rozmiar];
+            //wrzucanie liter dla odpowiednich wskażników 
+            for (int i = 0; i < klucz; i++)
+            {
+                for (int j = 0; j < lista[i].Count; j++)
+                {
+                    //wrzucanie liter w odpowiadające im miejsce | w bufer żeby można było wyciągnać z niego odrazu znaki
+                    buffer[lista[i][j]] = zakodowany[licznik];
+                    licznik++;
+                }
+            }
+            //new string bo rzutowanie bufera(char) na string
+            return new string(buffer);
+        }
+
+        protected void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = Decrypt(klucz, textBox3.Text);
+        }
+    }
+}
