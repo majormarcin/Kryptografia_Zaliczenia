0218eed0ff17109d36d03b4e611d07eaa2ed3862
MajorMarcin <zelkowski.m@gmail.com>
2018-11-27 09:33:15 +0100

    Kryptosystem Vigenere

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -36,26 +36,39 @@ namespace WindowsFormsApp1
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog2 = new System.Windows.Forms.SaveFileDialog();
+            this.saveFileDialog3 = new System.Windows.Forms.SaveFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(6, 19);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(114, 23);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Szukaj pliku";
+            this.button1.Text = "Wczytaj";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Location = new System.Drawing.Point(6, 45);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 2;
@@ -66,71 +79,162 @@ namespace WindowsFormsApp1
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 64);
+            this.textBox1.Location = new System.Drawing.Point(12, 165);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(468, 132);
+            this.textBox1.Size = new System.Drawing.Size(406, 91);
             this.textBox1.TabIndex = 3;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(132, 12);
+            this.button3.Location = new System.Drawing.Point(168, 19);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 4;
-            this.button3.Text = "szyfruj";
+            this.button3.Text = "Szyfruj";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(213, 12);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "odszyfruj";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(294, 15);
+            this.textBox2.Location = new System.Drawing.Point(259, 19);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(56, 20);
             this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(365, 22);
+            this.label2.Location = new System.Drawing.Point(168, 24);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.Size = new System.Drawing.Size(85, 13);
             this.label2.TabIndex = 7;
-            this.label2.Text = "Klucz:";
+            this.label2.Text = "Długość klucza:";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(12, 202);
+            this.textBox3.Location = new System.Drawing.Point(12, 350);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(468, 117);
+            this.textBox3.Size = new System.Drawing.Size(406, 82);
             this.textBox3.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(406, 73);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Klucz";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 13;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(87, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Zapisz";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(6, 19);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Wczytaj";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(321, 19);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Generuj";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(87, 19);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Zapisz";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button7);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 91);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(406, 68);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Plik";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(249, 19);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "Odszyfruj";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 262);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(404, 82);
+            this.textBox4.TabIndex = 11;
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(495, 335);
+            this.ClientSize = new System.Drawing.Size(428, 445);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Marcin Zelkowski";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -146,10 +250,21 @@ namespace WindowsFormsApp1
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog3;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -8,7 +8,11 @@ using System.Text;
 using System.Windows.Forms;
 using System.IO;
 
-
+//Encryption
+//The the plaintext(P) and key(K) are added modulo 26.
+//Ei = (Pi + Ki) mod 26
+//Decryption
+//Di = (Ei - Ki + 26) mod 26
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
@@ -21,6 +25,7 @@ namespace WindowsFormsApp1
         public string napis;
         private void button1_Click(object sender, EventArgs e)
         {
+            //wczytanie pliku
             if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
                 label1.Text = openFileDialog1.FileName;
                 byte[] fileBytes = File.ReadAllBytes(label1.Text);
@@ -29,7 +34,6 @@ namespace WindowsFormsApp1
                 textBox1.Text = napis.ToString();
             }
         }
-
         private void button2_Click(object sender, EventArgs e)
         {
             if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
@@ -37,40 +41,140 @@ namespace WindowsFormsApp1
                 File.WriteAllText(saveFileDialog1.FileName, textBox1.Text);
             }
         }
-        public int klucz;
-        String[] nowy;
         public void button3_Click(object sender, EventArgs e)
         {
-            klucz = System.Convert.ToInt32(textBox2.Text);
-            label2.Text = "Obecnie użyty klucz:"+textBox2.Text;
-            textBox3.Text = koduj(textBox1.Text);
-        }
-        protected string koduj(string napis_dokodowania)
-        {
-            string output = string.Empty;
-
-            foreach (char ch in napis_dokodowania)
-                output += znak(ch);
-
-            return output;
-        }
-        protected char znak(char znak) {
-            if (!char.IsLetter(znak))
+            if (textBox2.Text == "" && textBox1.Text == "")
             {
-            return znak;
+                MessageBox.Show("Nie podano klucza lub tekstu do zakodowania");
             }
-            char d = char.IsUpper(znak) ? 'A' : 'a';
-            return (char)((((znak + klucz) - d) % 26) + d);
+            else {
+            textBox3.Text = koduj(textBox1.Text, textBox4.Text);
+            }
+        }
+        //stworzenie długiego klucza (powtarzanie)
+        protected string dajklucz(string napis_dokodowania, string klucz)
+        {
+            int x = napis_dokodowania.Length;
+            for (int i = 0; ; i++)
+            {
+                if (x == i)
+                    i = 0;
+                if (klucz.Length == napis_dokodowania.Length)
+                    break;
+                klucz += klucz[i];
+            }
+            //zwraca długi klucz
+            return klucz;
+        }
+        protected string koduj(string napis_dokodowania, string klucz)
+        {
+            //zwraca długi klucz 
+            klucz = dajklucz(napis_dokodowania, klucz);
+            //długi klucz wskakuje odrazu na miejsce starego w polu tekstowym
+            textBox4.Text= dajklucz(napis_dokodowania, klucz);
+            
+            string zakodowany = "";
+            for (int i = 0; i < napis_dokodowania.Length; i++)
+            {
+                int x2 = (napis_dokodowania[i] + klucz[i]) % 256;
+                zakodowany += (char)x2;
+            }
+            return zakodowany;
         }
 
-        protected string decode(string napis_dokodowania)
+        private void button2_Click_1(object sender, EventArgs e)
         {
-            klucz = 26-klucz;
-            return koduj(napis_dokodowania);
+            //zapis klucza
+
+            saveFileDialog2.FileName = "klucz";
+            saveFileDialog2.Filter = "Dokument tekstowy (*.txt)|*.txt";
+            saveFileDialog2.ShowDialog();
+
+            string location;
+            location = saveFileDialog2.FileName;
+            File.WriteAllText(location, textBox4.Text);
         }
-        protected void button4_Click(object sender, EventArgs e)
+
+        private void button5_Click(object sender, EventArgs e)
         {
-            textBox3.Text = decode(textBox1.Text);
+            //generowanie klucza
+            if (textBox2.Text == "")
+            {
+                MessageBox.Show("Nie podano długości klucza");
+            }
+            else
+            {
+            int dlugosc_klucza = System.Convert.ToInt32(textBox2.Text);
+            label2.Text = "Obecnie użyty klucz:" + textBox2.Text;
+            generator(dlugosc_klucza);
+            }
+        }
+
+        private string generator(int dlugosc_klucza)
+        {
+            Random rand = new Random();
+            string klucz = "";
+            for(int i=0; i<=dlugosc_klucza; i++)
+            {
+                
+                klucz +=(char)rand.Next(31,256);
+            }
+            textBox4.Text = klucz;
+            return klucz;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //wczytywanie klucza
+            if (openFileDialog2.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                label3.Text = openFileDialog2.FileName;
+                byte[] fileBytes2 = File.ReadAllBytes(label3.Text);
+                string napis2 = Encoding.ASCII.GetString(fileBytes2);
+                foreach (byte b2 in fileBytes2)
+                    textBox4.Text = napis2.ToString();
+            }
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            //zapis pliku 
+            saveFileDialog1.FileName = "zakodowany";
+            saveFileDialog1.Filter = "Dokument tekstowy (*.txt)|*.txt";
+            saveFileDialog1.ShowDialog();
+
+            string location;
+            location = saveFileDialog1.FileName;
+            File.WriteAllText(location, textBox3.Text);
+        }
+        protected string dekoduj(string zakodowany, string klucz)
+        {
+
+            string odkodowany = "";
+
+            for (int i = 0; i < zakodowany.Length; i++)
+            {
+                int x = (zakodowany[i] - klucz[i] + 256) % 256;
+                odkodowany += (char)x;
+            }
+            return odkodowany;
+
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (textBox4.Text == "")
+            {
+                MessageBox.Show("Nie podano klucza");
+            }
+            else
+            {
+                textBox1.Text = dekoduj(textBox3.Text, textBox4.Text);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
